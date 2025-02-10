85a33a9719b526b3df07e330e379241d0187a483
Hy-InitiativeLtdMain <tobilobaojuolape@gmail.com>
2025-02-10 22:34:23 +0000

    feat: add email login

--- privasee-app/app/page.tsx
+++ privasee-app/app/page.tsx
@@ -2,10 +2,12 @@
 import {
   GithubSignInButton,
   GoogleSignInButton,
+  SigninWithEmail,
 } from "@/components/authButtons";
 import { getServerSession } from "next-auth";
 import { authConfig } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { SelectSeparator } from "@/components/ui/select";
 
 
 export default async function SignInPage() {
@@ -20,6 +22,15 @@ export default async function SignInPage() {
         <p>Login with any of the options below:</p>
         <GoogleSignInButton />
         <GithubSignInButton />
+
+       
+        <div className="inline-flex items-center justify-center w-full">
+          <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">or</span>
+        </div>
+      
+
+        <SigninWithEmail />
       </div>
     </div>
   );
